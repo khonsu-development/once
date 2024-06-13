acb855f2b15e7e0666dba4b3e67df58ef3a4e5e4
Johann Roux <johann.roux@ada.com>
2024-06-13 19:59:32 +0200

    Set minSdk to 21

--- once-example/build.gradle.kts
+++ once-example/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "eu.khonsu.onceexample"
-        minSdk = 19
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
